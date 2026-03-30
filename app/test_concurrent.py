9d565315f32c4953c21a19e825999cac60dc3e48
Jordà Coca <jordacrodriguez02@gmail.com>
2026-03-30 19:10:26 +0200

    Funciona correctamente d momento

--- app/test_concurrent.py
+++ app/test_concurrent.py
@@ -3,15 +3,18 @@ from concurrent.futures import ThreadPoolExecutor
 
 URL = "http://127.0.0.1:8000/buy"
 
+# 🔹 Reutilizamos la sesión para todos los requests
+session = requests.Session()
+
 def send(i):
-    r = requests.post(URL, json={
+    r = session.post(URL, json={
         "client_id": "c1",
         "request_id": str(i)
     })
     return r.json()
 
-NUM_REQUESTS = 1000
-NUM_WORKERS = 50
+NUM_REQUESTS = 25000
+NUM_WORKERS = 100
 
 with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
     results = list(executor.map(send, range(NUM_REQUESTS)))
