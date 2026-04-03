21ae7d44f7219b92104a003fb692d03e791a649a
Jordà Coca <jordacrodriguez02@gmail.com>
2026-04-03 18:15:28 +0200

    tweaked

--- app/autoscaler.py
+++ app/autoscaler.py
@@ -9,8 +9,8 @@ BASE_PORT = 8001
 
 # Variables donde definimos la carga por cada worker
 MIN_WORKERS = 1
-MAX_WORKERS = 8
-TARGET_RPS_PER_WORKER = 250
+MAX_WORKERS = 40
+TARGET_RPS_PER_WORKER = 145
 CHECK_INTERVAL = 1
 
 # Load Balancer endpoint
