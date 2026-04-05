79eff56a15f093100d305ba811ec5415eae323c9
Jordà Coca <jordacrodriguez02@gmail.com>
2026-04-05 18:57:25 +0200

    Para benchmarking

--- app/testing/generate_benchmarks.py
+++ app/testing/generate_benchmarks.py
@@ -1,6 +1,10 @@
 import random
 
 def generate_unnumbered(n):
+    """
+            Genera un benchmark de tipo 'unnumbered'.
+            n: número total de solicitudes (por ejemplo 2100)
+    """
     filename = f"benchmark_unnumbered_{n}.txt"
     with open(filename, "w") as f:
         for i in range(n):
@@ -9,6 +13,11 @@ def generate_unnumbered(n):
 
 
 def generate_numbered(n):
+    """
+        Genera un benchmark de tipo 'numbered'.
+        n: número total de solicitudes (por ejemplo 2100)
+        El numero de silla es asignado aleatoriamente dentro del rango entre 1 y numero de peticiones
+    """
     filename = f"benchmark_numbered_{n}.txt"
     with open(filename, "w") as f:
         for i in range(n):
@@ -16,6 +25,22 @@ def generate_numbered(n):
             f.write(f"BUY c{i} {seat} r{i}\n")
     print("Numbered benchmark guardado en", filename)
 
+def generate_numbered_stress(n, total_seats=210):
+    """
+    Genera un benchmark de tipo 'numbered' para stress testing.
+    Cada silla del rango 1..total_seats se repetirá de manera circular hasta completar n solicitudes.
+
+    n: número total de solicitudes (por ejemplo 2100)
+    total_seats: número de sillas disponibles (por defecto 210)
+    """
+    filename = f"benchmark_numbered_stress_{n}_{total_seats}.txt"
+    with open(filename, "w") as f:
+        for i in range(n):
+            seat = (i % total_seats) + 1  # va de 1 a total_seats y se repite
+            f.write(f"BUY c{i} {seat} r{i}\n")
+    print("Benchmark de stress guardado en", filename)
+
 if __name__ == "__main__":
     #generate_unnumbered(2100)
     generate_numbered(2100)
+    generate_numbered_stress(2100)
